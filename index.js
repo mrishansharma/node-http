7c1ab190e716fd5513279f04725a38eeacacde91
mrishansharma <ishansharma501@gmail.com>
2020-07-05 11:49:09 +0530

    setting up simple node http server which serves static files from server side

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 const http = require('http');
-
+const fs = require('fs');
+const path = require('path');
 const hostname = 'localhost';
 const port = '3000';
 
 const server = http.createServer((req,resp)=>
     {
-        console.log(req.headers);
+        console.log('Request for ' + req.url + 'by method' + req.method);
         
         
-        resp.statusCode = 200;
-        resp.setHeader('Content-Type', 'text/html');
-        resp.end('<html><body><h1>Hello World! Message from Ishan</h1></body></html>');
+        if(req.method=== 'GET')
+        {
+            var fileUrl;
+            if(req.url == '/') fileUrl = '/index.html'
+            else fileUrl = req.url;
+            
+            var filePath = path.resolve('./public'+fileUrl );
+            const fileExt = path.extname(filePath);
+            if(fileExt==='.html'){
+                fs.exists(filePath,(exists)=>
+                    {
+                        if(!exists){
+                            resp.statusCode = 404;
+                            resp.setHeader('Content-Type','text/html');
+                            resp.end ('<html><body>Error 404 '+fileUrl+'not found </body></html>');
+                            return;
+                        }   
+                        resp.statusCode = 200;
+                        resp.setHeader("Content-Type", 'text/html');
+                        //take the file and construct it into response
+                        fs.createReadStream(filePath).pipe(resp);
+                 });
+
+            }
+            else{
+                resp.statusCode = 404;
+                resp.setHeader('Content-Type','text/html');
+                resp.end ('<html><body>Error 404 '+fileUrl+' not a html file </body></html>');
+                return;
+            }
+        }
+        else{
+            resp.statusCode = 404;
+            resp.setHeader('Content-Type','text/html');
+            resp.end ('<html><body>Error 404 '+req.method+' req method not supported  </body></html>');
+            return; 
+        }
     });
 
 server.listen(port,hostname, ()=>
     {
-            console.log(`Server running at http://${hostname}:${port}`)
-    })
+            console.log(`Server running at http://${hostname}:${port    }`)
+    });
