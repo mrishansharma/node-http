00984a1e1541ec9635d7735dd63c3d44f0d513e6
mrishansharma <ishansharma501@gmail.com>
2020-07-05 11:01:01 +0530

    creating node-http practice project

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = '3000';
+
+const server = http.createServer((req,resp)=>
+    {
+        console.log(req.headers);
+        
+        
+        resp.statusCode = 200;
+        resp.setHeader('Content-Type', 'text/html');
+        resp.end('<html><body><h1>Hello World! Message from Ishan</h1></body></html>');
+    });
+
+server.listen(port,hostname, ()=>
+    {
+            console.log(`Server running at http://${hostname}:${port}`)
+    })
